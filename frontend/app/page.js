5f095a21e08eb4840f31c267860ea87679ba11c6
Charlie Holtz <charlieholtz@gmail.com>
2023-05-04 17:08:39 -0400

    remove boilerplate

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -18,17 +18,6 @@ export default function Home() {
           </a>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
     </main>
   );
 }
